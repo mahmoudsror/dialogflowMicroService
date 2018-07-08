6018b0d759bb22e3eca373854b2e82cc5b04b611
mahmoud <mahmoud@beeinteractivemena.com>
2018-07-08 15:34:18 +0200

    apigateway config

--- Bee/Bot/ApiGateway/index.js
+++ Bee/Bot/ApiGateway/index.js
@@ -4,5 +4,5 @@ const app= express()
 const config = require(path.resolve('config','index.js'))
 
 var listener = app.listen(config.PORT||5000,()=>{
-    console.log("Welcome to Bot API-Gateway , listen to %s",listener.address().port)
+    console.log("Welcome to Facebook Messenger Bot API-Gateway , listen to %s",listener.address().port)
 })
